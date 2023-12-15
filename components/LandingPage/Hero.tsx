dfa5e316a4973f416829b323ad19b6e601676188
AYENI DANIEL <starlordflash@gmail.com>
2023-12-15 10:51:13 +0100

    fixed now

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -37,7 +37,8 @@ const Hero = () => {
           data-aos-duration="2000"
         >
           <h1 className="font-bold text-white text-[28px] sm:text-[28px] lg:text-[40px] 2xl:text-[64px] text-start lg:w-9/12 2xl:w-9/12 2xl:leading-[88px] block mt-8 2xl:mt-[100px] tracking-[-2%] nuto max-w-[654px] ">
-            African Centre for Innovation, Education and Development
+            African Centre for Inclusive Entrepreneurship Resilience and Social
+            Innovation
           </h1>
           <p className="xl:text-xl  text-white text-base font-light text-start mt-6 ">
             learn more about who we are and what we do
@@ -52,17 +53,29 @@ const Hero = () => {
           </div>
           <div className="flex mt-12 space-x-[10px]">
             <span
-              className={`${currentImageIndex === 0 ? "bg-[#fe50061a] border-[0.5px] border-orange text-orange" : "bg-transparent text-white border border-white"} w-[34px] h-[34px] rounded-full flex items-center justify-center  `}
+              className={`${
+                currentImageIndex === 0
+                  ? "bg-[#fe50061a] border-[0.5px] border-orange text-orange"
+                  : "bg-transparent text-white border border-white"
+              } w-[34px] h-[34px] rounded-full flex items-center justify-center  `}
             >
               1
             </span>
             <span
-              className={`${currentImageIndex === 1 ? "bg-[#fe50061a] border-[0.5px] border-orange text-orange" : "bg-transparent text-white border border-white"} w-[34px] h-[34px] rounded-full flex items-center justify-center  `}
+              className={`${
+                currentImageIndex === 1
+                  ? "bg-[#fe50061a] border-[0.5px] border-orange text-orange"
+                  : "bg-transparent text-white border border-white"
+              } w-[34px] h-[34px] rounded-full flex items-center justify-center  `}
             >
               2
             </span>
             <span
-              className={`${currentImageIndex === 2 ? "bg-[#fe50061a] border-[0.5px] border-orange text-orange" : "bg-transparent text-white  border-white border-[0.5px]  " } w-[34px] h-[34px] rounded-full flex items-center justify-center  `}
+              className={`${
+                currentImageIndex === 2
+                  ? "bg-[#fe50061a] border-[0.5px] border-orange text-orange"
+                  : "bg-transparent text-white  border-white border-[0.5px]  "
+              } w-[34px] h-[34px] rounded-full flex items-center justify-center  `}
             >
               3
             </span>
---
+++ components/socio/Brief.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContainerLayout from '../../Layouts/ContainerLayout'
+import picA from "../../assets/png/socio-a.png"
+import picB from "../../assets/png/socio-b.png"
+import Image from 'next/image'
+const Brief = () => {
+  return (
+    <div className='w-full '>
+          <ContainerLayout>
+              <div className="w-full flex flex-col">
+                  
+              </div>
+      </ContainerLayout>
+    </div>
+  )
+}
+
+export default Brief
